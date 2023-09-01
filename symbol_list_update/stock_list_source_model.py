c9b38322f799213d2518346155afd4e7061becfa
Chris Sze <8147803+gate-max@users.noreply.github.com>
2023-09-02 00:42:37 +0800

    pro

--- symbol_list_update/stock_list_source_model.py
+++ symbol_list_update/stock_list_source_model.py
@@ -1,12 +1,14 @@
 '''
+*** INDEPENDENT MODULE ***
 
+IMPORTED BY: 
+    stock_guru_update/
 
 '''
 
 # STANDARD LIBS
 import sys;sys.path.append('..')
 import io
-from datetime import date
 
 from typing import Any, Dict, List, Set, Union
 import urllib.request as request
@@ -17,43 +19,61 @@ from bs4 import BeautifulSoup, ResultSet
 import pandas 
 from pandas.core.frame import DataFrame
 import requests
-from requests.models import Response
 
 # CUSTOM LIBS
-from batterypy.string.read import is_floatable, readf
+from dimsumpy.web.crawler import get_html_dataframes, get_html_soup
 
 # PROGRAM MODULES
-from 
+
 
 def get_sp_500() -> List[str]:
-    r: Response = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
-    dfs: List[DataFrame] = pandas.read_html(r.text, header=0)
+    '''
+    * INDEPENDENT *
+    IMPORTS: dimsumpy
+    '''
+    sp_500_url: str = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+    dfs: List[DataFrame] = get_html_dataframes(sp_500_url)
     stocks: List[str] = list(dfs[0].iloc[0:, 0])
     return stocks
 
 def try_get_sp_500() -> List[str]:
+    '''
+    DEPENDS ON: get_sp_500()
+    '''
     try:
-        r: Response = requests.get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
-        dfs: List[DataFrame] = pandas.read_html(r.text, header=0)
-        stocks: List[str] = list(dfs[0].iloc[0:, 0])
+        stocks: List[str] = get_sp_500()
         return stocks
-    except requests.exceptions.RequestException as e:
-        print('getsp500 error e:', e)
+    except requests.exceptions.RequestException as requests_error:
+        print('try_get_sp_500 RequestException:', requests_error)
         return []
-    except Exception as e2:
-        print('get_sp_500 error e2:', e2)
+    except Exception as error:
+        print('try_get_sp_500 general Exception:', error)
         return []
 
 
 
 
 def get_nasdaq_100() -> List[str]:
+    '''
+    * INDEPENDENT *
+    IMPORTS: pandas, dimsumpy
+    '''
+    nasdaq_100_url: str = 'https://en.wikipedia.org/wiki/NASDAQ-100'
+    soup: BeautifulSoup = get_html_soup(nasdaq_100_url)
+    soup_item: ResultSet = soup.find('table', id='constituents')
+    dfs: List[DataFrame] = pandas.read_html(str(soup_item), header=0)
+    stocks: List[str] = list(dfs[0].iloc[0:, 1])
+    return stocks
+
+
+
+
+def try_get_nasdaq_100() -> List[str]:
+    '''
+    DEPENDS ON: get_nasdaq_100()
+    '''
     try:
-        r: Response = requests.get("https://en.wikipedia.org/wiki/NASDAQ-100")
-        soup: BeautifulSoup = BeautifulSoup(r.text, 'html.parser')
-        soup_item: ResultSet = soup.find('table', id='constituents')
-        dfs: List[DataFrame] = pandas.read_html(str(soup_item), header=0)
-        stocks: List[str] = list(dfs[0].iloc[0:, 1])
+        stocks: List[str] = get_nasdaq_100()
         return stocks
     except requests.exceptions.RequestException as e:
         print('get_nasdaq_100 error e:', e)
@@ -64,8 +84,12 @@ def get_nasdaq_100() -> List[str]:
 
 
 
-
 def get_nasdaq_listed() -> List[str]:
+    '''
+    * INDEPENDENT *
+    IMPORTS: urllib, pandas
+    '''
+
     try:
         url1 = 'ftp://ftp.nasdaqtrader.com/SymbolDirectory/nasdaqlisted.txt'
         with request.urlopen(url1) as r:
@@ -79,6 +103,10 @@ def get_nasdaq_listed() -> List[str]:
 
 
 def get_nasdaq_traded() -> List[str]:
+    '''
+    * INDEPENDENT *
+    IMPORTS: urllib, pandas
+    '''
     try:
         url1 = 'ftp://ftp.nasdaqtrader.com/SymbolDirectory/nasdaqtraded.txt'
         with request.urlopen(url1) as r:
@@ -92,7 +120,11 @@ def get_nasdaq_traded() -> List[str]:
 
 
 def get_option_traded() -> List[str]:
-    """ file size is about 50mb, unique symbols are about 3129"""
+    '''
+    * INDEPENDENT *
+    IMPORTS: urllib, pandas
+    file size is about 50mb, need several minutes to process, unique symbols are about 3129
+    '''
     try:
         url1 = 'ftp://ftp.nasdaqtrader.com/SymbolDirectory/options.txt'
         with request.urlopen(url1) as r:
@@ -104,11 +136,6 @@ def get_option_traded() -> List[str]:
     except Exception as e:
         print('get_option_traded error:', e)
         return []
-
-
-
-
-
 
 
 if __name__ == '__main__':
