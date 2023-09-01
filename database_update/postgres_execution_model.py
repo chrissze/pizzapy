ff78afc4d03c6f619e2b943e3145a277b67f8f14
CS <you@example.com>
2023-09-01 13:33:13 +0800

    pro

--- database_update/postgres_execution_model.py
+++ database_update/postgres_execution_model.py
@@ -9,7 +9,7 @@ Prerequites of Postgres Server connection:
 SQL commands format in execute_pandas_read() and execute_psycopg_command():
     (1) Optional to add semicolon add the end. That is, I can omit it.
 
-    
+
     
 '''
 
@@ -32,33 +32,48 @@ from postgres_connection_model import execute_pandas_read, execute_psycopg_comma
         
 
 
-def create_new_postgres_db():
+def show_databases() -> DataFrame:
     '''
     * INDEPENDENT *
+    IMPORTS: execute_pandas_read()
+    I can delete a database in psql, no need to create drop_database
+    because there should be a few databases only.
+    '''
+    cmd: str = "SELECT datname FROM pg_database WHERE datistemplate = false"
+    df: DataFrame = execute_pandas_read(cmd)
+    return df
+
+
+def create_new_postgres_db() -> None:
+    '''
+    DEPENDS ON: show_databases()
     IMPORTS: subprocess
     '''
-    new_database_name = input('Please input the desired database name then press ENTER: ')
-    reply = input(f'Do you want to create a new database in Postgresql - {new_database_name} (y/N)? ')
-    if reply == 'y':
+    print('\nCurrent available databases in Postgresql: \n')
+    print(show_databases())
+    initial_reply = input(f'\n\nDo you want to create a new database in Postgresql (y/N)? ')
+    if initial_reply != 'y':
+        return
+    new_database_name = input('\nPlease input the desired database name then press ENTER: ')
+    confirmation_reply = input(f'\nDo you want to create a new database - {new_database_name} (y/N)? ')
+    if confirmation_reply == 'y':
         createdb_cmd = f'createdb {new_database_name} -U postgres'
         subprocess.run(createdb_cmd, stdin=True, shell=True)
         print(f'Just created a database -- {new_database_name}')
+        print(show_databases())
     else:
         print('no database is created')
 
 
-
-
-def describe_table(table_name: str) -> DataFrame:
+def show_table(table_name: str) -> DataFrame:
     '''
     * INDEPENDENT *
     IMPORTS: execute_pandas_read() 
-    CALLED BY: show_single_table()
+    CALLED BY: loop_show_table()
     '''
-    describe_table_cmd: str = f"SELECT column_name, data_type, character_maximum_length from INFORMATION_SCHEMA.COLUMNS WHERE table_name = '{table_name}';"
-    dataframe: DataFrame = execute_pandas_read(describe_table_cmd)
-    return dataframe
-
+    cmd: str = f"SELECT column_name, data_type, character_maximum_length from INFORMATION_SCHEMA.COLUMNS WHERE table_name = '{table_name}';"
+    df: DataFrame = execute_pandas_read(cmd)
+    return df
 
 
 def show_tables() -> DataFrame:
@@ -71,18 +86,22 @@ def show_tables() -> DataFrame:
     return df
 
 
-def show_single_table() -> None:
+
+
+
+
+def loop_show_table() -> None:
     '''
-    DEPENDS ON: show_tables(), describe_table()
+    DEPENDS ON: show_tables(), show_table()
     IMPORTS: db_table_command_dict
     '''
     while True:
-        show_tables()
-        table: str = input('Input table name (0 to quit): ')
-        if table == '0':
+        print(show_tables())
+        table_name: str = input('Input table name (0 to quit): ')
+        if table_name == '0':
             break
-        elif table in db_table_command_dict:
-            describe_table(table)
+        elif table_name in db_table_command_dict:
+            print(show_table(table_name))
         else:
             print('invalid table name')
 
@@ -90,26 +109,16 @@ def show_single_table() -> None:
 
 
 
-def show_databases() -> DataFrame:
-    '''
-    * INDEPENDENT *
-    IMPORTS: execute_pandas_read()
-    '''
-    cmd: str = "SELECT datname FROM pg_database WHERE datistemplate = false"
-    df: DataFrame = execute_pandas_read(cmd)
-    return df
-
-
 
 def create_table(table_name:str) -> None:
     '''
-    DEPENDS ON: describe_table()
+    DEPENDS ON: show_table()
     IMPORTS: db_table_command_dict, execute_psycopg_command()
     '''
     if table_name in db_table_command_dict:
         cmd: str = db_table_command_dict[table_name].get('command')
         execute_psycopg_command(cmd)
-        describe_table(table_name)
+        show_table(table_name)
     else:
         print('invalid table name')
 
@@ -121,7 +130,7 @@ def drop_table():
     IMPORTS:  db_table_command_dict, execute_psycopg_command()
     '''
     while True:
-        table_name: str = input("\n\nWhich table do you want to DROP? input the table name or '0' to cancel: ")
+        table_name: str = input("\nWhich table do you want to DROP? input the table name or '0' to cancel: ")
         if table_name == '0':
             break
         elif table_name in db_table_command_dict:
@@ -138,7 +147,7 @@ if __name__ == '__main__':
     cmd2 = 'SELECT 2+2'
     cmd3 = 'SELECT version()'
     
-    x = show_tables()    
+    x = show_table('em9')
     print(x)
 
     print(f'{__file__} done')
