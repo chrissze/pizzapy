09e034d00b0742fe1b757c255f99c9dc31d92a76
CS <you@example.com>
2023-09-14 20:20:08 +0800

    pro

--- stock_price_update/price_update_controller.py
+++ stock_price_update/price_update_controller.py
@@ -1,13 +1,21 @@
 """
 
+DEPENDS ON: price_update_view.py
+
+I can click the quit button to stop the current operations.
+
+I can start a 2nd operation when the 1st operation is still running by using threads.
+
+
 """
 
 
 # STANDARD LIBS
 import sys; sys.path.append('..')
 from datetime import date
+from itertools import dropwhile
 import time
-from typing import Any, Dict, Generator, List, Optional
+from typing import Any, Dict, Generator, List, Optional, Tuple
 
 
 # THIRD PARTY LIBS
@@ -18,12 +26,11 @@ from PySide6.QtWidgets import QApplication, QMessageBox
 
 # CUSTOM LIBS
 from batterypy.control.trys import try_str
-from batterypy.string.read import is_intable, int0, float0
+from batterypy.string.read import is_floatable, is_intable, int0, float0
 from dimsumpy.qt.decorators import confirmation_self
 
 
 # PROGRAM MODULES
-from core_stock_update.core_update_view import CoreUpdateView
 from database_update.stock_list_model import stock_list_dict, table_function_dict
 
 from stock_price_update.price_update_view import PriceUpdateView
@@ -32,6 +39,9 @@ from stock_price_update.price_update_database_model import upsert_price
 
 
 class CustomThread(QThread):
+    """
+    QThread is the base class.
+    """
     def __init__(self, func, callback, args=()) -> None:
         super().__init__()
         self.func = func
@@ -44,57 +54,124 @@ class CustomThread(QThread):
 
     def run(self) -> None:
         self.func(*self.args)
+
     def callitback(self) -> None:
         self.callback()
         print('callback 35') # debug
 
 
+
+
+
+def thread_finished(self, thread_id: float) -> None:
+    """
+    this is the callback function of CustomThread class
+    """
+    print(self.threads_dict, f' thread_finished() {thread_id} starts') # debug
+    self.threads_dict.pop(thread_id, None) # None is the default return value to prevent key error, pop means remove the item from dict
+    print(self.threads_dict, f' thread_finished() {thread_id} ends') # debug
+    thread_message: str = f' thread_finished() {thread_id} ends'
+    self.browser.append(thread_message)
+
+def get_calendar_dates(self) -> Tuple[date, date]:
+    """
+    * INDEPENDENT *
+    USED BY: update_price()
+    """
+    FROM: date = self.from_calendar.selectedDate().toPython()
+    TO: date = self.to_calendar.selectedDate().toPython()
+    return FROM, TO
+
+
+def call_upsert(self, FROM: date, TO: date, stockgen: Generator[str, None, None]) -> None:  
+    
+    """
+    IMPORTS: upsert_price()
+    USED BY: update_price()
+    
+    # return type is not None
+    This func runs in the QThread
+    
+    self.browser.repaint() will have error as multiple threads paint at the same time
+    self.browser.append(browser_text) will have paint errors.
+    """
+    for i, symbol in enumerate(stockgen, start=1): # if the list is too long, program will have Segmentation fault
+        result = try_str(upsert_price, FROM, TO, symbol)
+        browser_text: str = f"{i} / {self.list_length} {symbol} {result}"
+        self.progress_bar.setValue(i)
+        self.progress_label.setText(f'{i} / {self.list_length} {symbol}       ')
+        QCoreApplication.processEvents()  # update the GUI
+
+
+
+@confirmation_self
+def update_price(self) -> None:
+    """
+    DEPENDS ON: self.calendar_get_dates(), self.call_upsert(), CustomThread class, self.thread_finished()
+    """
+    FROM, TO = self.get_calendar_dates()
+    sender = self.sender().accessibleName()
+    if sender == 'update_price_list_button':
+        lineedit_text: str = self.start_lineedit.text()
+        stockstr = self.stock_list_combobox.currentText()
+        stocklist = stock_list_dict.get(stockstr)
+        if lineedit_text in stocklist:
+            stocklist = list(dropwhile(lambda x: x != lineedit_text, stocklist))
+        else:        
+            start_num = max(int0(lineedit_text) - 1, 0)
+            stocklist = stocklist[start_num:]
+
+    else:
+        stockstr = self.symbols_lineedit.text()
+        stocklist = stockstr.split()
+    self.list_length = len(stocklist)
+    self.progress_bar.setRange(0, self.list_length)
+    stockgen = (x for x in stocklist)
+    self.thread_id: float = time.time()
+    thread: CustomThread = CustomThread(self.call_upsert, lambda tid=self.thread_id: self.thread_finished(tid)
+                            , [FROM, TO, stockgen])
+    self.threads_dict[self.thread_id] = thread # added this line in 2019
+    thread.start()  # start the run() in QThread
+    thread.wait(2) # prevent crash
+    self.browser.append(f'{self.list_length} stocks')
+
+
+
+class MakeUpdateConnects:
+    def __init__(ego, self) -> None:
+        self.update_price_button.clicked.connect(self.update_price)
+        self.update_price_list_button.clicked.connect(self.update_price)  # update prices from a stock list
+
+
+
+
+
 class PriceUpdateController(PriceUpdateView):
+    """
+    By using QThread, I can still click the quit button to stop the operation when the update is running.
+
+    A single thread is used for the looping for the whole stock list.
+    The key in the threads_dict is a float, this key is created by time.time() in self.update_price()
+
+    self.update_price() is the main function, while get_calendar_dates(), call_upsert() and thread_finished() are helper functions of update_price().
+    """
     def __init__(self) -> None:
         super().__init__()
-        self.threads: Dict[float, CustomThread] = {}
-        self.b_price1s.clicked.connect(self.yahooupsert)
-        self.b_prices.clicked.connect(self.yahooupsert)  # update prices from a stock list
+        self.threads_dict: Dict[float, CustomThread] = {}
+        MakeUpdateConnects(self)
 
-    def threadfinished(self, thread_id: float) -> None:
-        print(self.threads, ' finished 46') # debug
-        self.threads.pop(thread_id, None) # None is the default return value to prevent key error, pop means remove the item from dict
-        print(self.threads, ' finished 48') # debug
+        
+    def get_calendar_dates(self) -> Tuple[date, date]:
+        return get_calendar_dates(self)
 
-    def yahooupsertfunc(self, date1: date, date2: date, stockgen: Generator[str, None, None]) -> None:  # return type is not None
-        """This func runs in the QThread"""
-        for count, symbol in enumerate(stockgen, start=1): # if the list is too long, program will have Segmentation fault
-            s = upsert_price(date1, date2, symbol)
-            msg: str = f"{count} / {self.l}  {s}"
-            self.pbar.setValue(count)
-            self.browser.append(msg)
-            print(msg)
-            QCoreApplication.processEvents()  # update the GUI
-            #self.browser.repaint() # this line will have error as multiple threads paint at the same time
+    def call_upsert(self, FROM: date, TO: date, stockgen: Generator[str, None, None]) -> None:  
+        return call_upsert(self, FROM, TO, stockgen)  
+        
+    def thread_finished(self, thread_id: float) -> None:
+        return thread_finished(self, thread_id)
 
-
-
-    @confirmation_self
-    def yahooupsert(self) -> None:
-        date1, date2 = self.calendar_get_dates()
-        sender = self.sender().accessibleName()
-        if sender == 'b_prices':
-            stockstr = self.combo.currentText()
-            start_num = int0(self.le_start.text())
-            stocklist = stock_list_dict.get(stockstr)[start_num:]
-        else:
-            stockstr = self.le.text()
-            stocklist = stockstr.split()
-        self.l = len(stocklist)
-        self.pbar.setRange(0, self.l)
-        stockgen = (x for x in stocklist)
-        thread_id: float = time.time()
-        thread: CustomThread = CustomThread(self.yahooupsertfunc, lambda tid=thread_id: self.threadfinished(tid)
-                              , [date1, date2, stockgen])
-        self.threads[thread_id] = thread # added this line in 2019
-        thread.start()  # start the run() in QThread
-        thread.wait(2) # prevent crash
-        self.browser.append(str(self.l) + ' stocks')
+    def update_price(self) -> None:
+        return update_price(self)
 
 
 
--- stock_price_update/price_update_view.py
+++ stock_price_update/price_update_view.py
@@ -1,12 +1,16 @@
+"""
 
+USED BY: price_update_controller.py
+
+"""
 # STANDARD LIBS
 import sys; sys.path.append('..')
-
-from database_update.stock_list_model import stock_list_dict
-
-
-
 from datetime import date
+from typing import Tuple
+
+
+
+# THIRD PARTY LIBS
 from PySide6.QtCore import QDate
 from PySide6.QtGui import QCloseEvent
 from PySide6.QtWidgets import (QApplication, QCalendarWidget, QComboBox, QGridLayout,
@@ -14,70 +18,19 @@ from PySide6.QtWidgets import (QApplication, QCalendarWidget, QComboBox, QGridLa
                                QProgressBar, QPushButton,
                                QTextBrowser, QVBoxLayout, QWidget)
 
+# CUSTOM LIBS
+from dimsumpy.qt.functions import closeEvent
 
-from typing import Tuple
+# PROGRAM MODULES
+from database_update.stock_list_model import stock_list_dict
 
 
 
-
-class CalendarGrid:
+class SetupWindow:
     def __init__(ego, self) -> None:
-
-        self.lb_cal1 = QLabel('')
-        self.b_reset1 = QPushButton('Reset Calendar 1')
-        self.b_reset1.setAccessibleName('b_reset1')
-        self.lb_cal2 = QLabel('')
-        self.b_reset2 = QPushButton('Reset Calendar 2')
-        self.b_reset2.setAccessibleName('b_reset2')
-
-
-        self.cal1 = QCalendarWidget()
-        self.cal1.setAccessibleName('cal1')  # same name as variable
-        self.cal2 = QCalendarWidget()
-        self.cal2.setAccessibleName('cal2')
-
-        grid1 = QGridLayout()
-        grid1.addWidget(self.lb_cal1,  0, 0)
-        grid1.addWidget(self.b_reset1, 0, 1)
-        grid1.addWidget(self.lb_cal2,  0, 2)
-        grid1.addWidget(self.b_reset2, 0, 3)
-
-        # grid (widget, row, column, height, width)
-        grid1.addWidget(self.cal1, 1, 0, 2, 2)
-        grid1.addWidget(self.cal2, 1, 2, 2, 2)
-        self.mainbox.addLayout(grid1)
-
-
-class UpdateGrid:
-    def __init__(ego, self) -> None:
-
-        self.combo = QComboBox()
-        self.combo.addItems(stock_list_dict.keys())
-        self.le_start = QLineEdit()
-        self.b_prices = QPushButton('Update Y! Prices')
-        self.b_prices.setAccessibleName('b_prices')
-        self.b_techs = QPushButton('Update Technicals')
-        self.b_techs.setAccessibleName('b_techs')
-
-        self.le = QLineEdit()
-        self.symbols_label = QLabel('Separate symbols by space')
-        self.b_price1s = QPushButton('Update Y! 1s')
-        self.b_price1s.setAccessibleName('b_price1s')
-        self.b_tech1s = QPushButton('Update Technical 1s')
-        self.b_tech1s.setAccessibleName('b_tech1s')
-
-        grid2 = QGridLayout()
-        grid2.addWidget(self.combo,    0, 0)
-        grid2.addWidget(self.le_start,    0, 1)
-        grid2.addWidget(self.b_prices, 0, 2)
-        grid2.addWidget(self.b_techs,  0, 3)
-
-        grid2.addWidget(self.le,       1, 0)
-        grid2.addWidget(self.symbols_label,    1, 1)
-        grid2.addWidget(self.b_price1s,1, 2)
-        grid2.addWidget(self.b_tech1s, 1, 3)
-        self.mainbox.addLayout(grid2)
-
+        self.setWindowTitle('Stock Price Update')
+        self.setGeometry(0, 0, 900, 900)
+        self.mainbox = QVBoxLayout(self)
 
 class BrowserRow:
     def __init__(ego, self) -> None:
@@ -87,143 +40,178 @@ class BrowserRow:
         self.mainbox.addWidget(self.browser)
 
 
+class CalendarGrid:
+    def __init__(ego, self) -> None:
+
+        self.from_calendar_label = QLabel('')
+        self.from_reset_button = QPushButton('Reset FROM date')
+        self.from_reset_button.setAccessibleName('from_reset_button')
+        self.to_calendar_label = QLabel('')
+        self.to_reset_button = QPushButton('Reset TO date')
+        self.to_reset_button.setAccessibleName('to_reset_button')
+
+
+        self.from_calendar = QCalendarWidget()
+        self.from_calendar.setAccessibleName('from_calendar')  # same name as variable
+        self.to_calendar = QCalendarWidget()
+        self.to_calendar.setAccessibleName('to_calendar')
+
+        calendar_grid = QGridLayout()
+        calendar_grid.addWidget(self.from_calendar_label,  0, 0)
+        calendar_grid.addWidget(self.from_reset_button, 0, 1)
+        calendar_grid.addWidget(self.to_calendar_label,  0, 2)
+        calendar_grid.addWidget(self.to_reset_button, 0, 3)
+
+        # grid (widget, row, column, height, width)
+        calendar_grid.addWidget(self.from_calendar, 1, 0, 2, 2)
+        calendar_grid.addWidget(self.to_calendar, 1, 2, 2, 2)
+        self.mainbox.addLayout(calendar_grid)
+
+
+class UpdateGrid:
+    def __init__(ego, self) -> None:
+
+        self.stock_list_combobox = QComboBox()
+        self.stock_list_combobox.addItems(stock_list_dict.keys())
+        self.start_lineedit = QLineEdit()
+        self.start_lineedit.setPlaceholderText('Input a number from 1 OR a SYMBOL in the list to start')
+        self.update_price_list_button = QPushButton('Update Price List')
+        self.update_price_list_button.setAccessibleName('update_price_list_button')
+        self.update_technical_list_button = QPushButton('Update Technical List')
+        self.update_technical_list_button.setAccessibleName('update_technical_list_button')
+
+        self.symbols_lineedit = QLineEdit()
+        self.symbols_label = QLabel('Separate symbols by space')
+        self.update_price_button = QPushButton('Update Price')
+        self.update_price_button.setAccessibleName('update_price_button')
+        self.update_technical_button = QPushButton('Update Technical')
+        self.update_technical_button.setAccessibleName('update_technical_button')
+
+        update_grid = QGridLayout()
+        update_grid.addWidget(self.stock_list_combobox,    0, 0)
+        update_grid.addWidget(self.start_lineedit,    0, 1)
+        update_grid.addWidget(self.update_price_list_button, 0, 2)
+        update_grid.addWidget(self.update_technical_list_button,  0, 3)
+
+        update_grid.addWidget(self.symbols_lineedit,       1, 0)
+        update_grid.addWidget(self.symbols_label,    1, 1)
+        update_grid.addWidget(self.update_price_button,1, 2)
+        update_grid.addWidget(self.update_technical_button, 1, 3)
+        self.mainbox.addLayout(update_grid)
+
+
+
 class ProgressRow:
     def __init__(ego, self) -> None:
 
-        self.pbar = QProgressBar()
-        self.b_clear = QPushButton('Clear')
-        self.b_quit = QPushButton('Quit')
+        self.progress_bar = QProgressBar()
+        self.progress_label = QLabel(' 0 / 0          ')
+        self.clear_button = QPushButton('Clear')
+        self.quit_button = QPushButton('Quit')
         hbox = QHBoxLayout()
-        hbox.addWidget(self.pbar)
-        hbox.addWidget(self.b_clear)
-        hbox.addWidget(self.b_quit)
+        hbox.addWidget(self.progress_bar)
+        hbox.addWidget(self.progress_label)
+        hbox.addWidget(self.clear_button)
+        hbox.addWidget(self.quit_button)
         self.mainbox.addLayout(hbox)
 
 
 
-class MakeConnects:
-    def __init__(ego, self) -> None:
-
-        self.b_reset1.clicked.connect(self.calendar_reset)
-        self.b_reset2.clicked.connect(self.calendar_reset)
-        self.b_reset1.click()
-        self.b_reset2.click()
-
-        self.cal1.clicked[QDate].connect(self.calendar_show_date)
-        self.cal2.clicked[QDate].connect(self.calendar_show_date)
-
-        self.cal1.currentPageChanged.connect(self.cal1.repaint)
-        self.cal2.currentPageChanged.connect(self.cal2.repaint)
-
-        self.b_clear.clicked.connect(self.browser.clear)
-        self.b_quit.clicked.connect(self.close)
-
-
-def closeEvent(self, event: QCloseEvent) -> None:
-    reply: QMessageBox.StandardButton = QMessageBox.question(
-        self, 'Confirmation', 'Quit Now?', QMessageBox.Yes | QMessageBox.Cancel)
-    print(type(reply))
-    if reply == QMessageBox.Yes:
-        event.accept()
-    else:
-        event.ignore()
-
-
-def calendar_reset(self) -> None:
+def reset_calendar(self) -> None:
     sender: str = self.sender().accessibleName()
     today_: date = date.today()
     qtoday: QDate = QDate.fromString(str(today_), 'yyyy-MM-dd')
     qtodaystr: str = qtoday.toString()
-    if sender == 'b_reset1':
-        self.cal1.setSelectedDate(qtoday)
-        self.cal1.updateCells()
-        self.lb_cal1.setText(qtodaystr)
-        self.lb_cal1.repaint()
-    elif sender == 'b_reset2':
-        self.cal2.setSelectedDate(qtoday)
-        self.cal2.updateCells()
-        self.lb_cal2.setText(qtodaystr)
-        self.lb_cal2.repaint()
+    if sender == 'from_reset_button':
+        self.from_calendar.setSelectedDate(qtoday)
+        self.from_calendar.updateCells()
+        self.from_calendar_label.setText(qtodaystr)
+        self.from_calendar_label.repaint()
+    elif sender == 'to_reset_button':
+        self.to_calendar.setSelectedDate(qtoday)
+        self.to_calendar.updateCells()
+        self.to_calendar_label.setText(qtodaystr)
+        self.to_calendar_label.repaint()
     else:
         print('no sender')
     QApplication.processEvents()
 
 
 
-def calendar_show_date(self, qdate: QDate) -> None:
+def update_calendar_date(self, qdate: QDate) -> None:
     sender: str = self.sender().accessibleName()
     datestr: str = qdate.toString()
-    if sender == 'cal1':
-        self.lb_cal1.setText(datestr)
-        self.lb_cal1.repaint()
-        self.cal1.updateCells()
-        self.cal1.repaint()
-    elif sender == 'cal2':
-        self.lb_cal2.setText(datestr)
-        self.lb_cal2.repaint()
-        self.cal2.updateCells()
-        self.cal2.repaint()
+    if sender == 'from_calendar':
+        self.from_calendar_label.setText(datestr)
+        self.from_calendar_label.repaint()
+        self.from_calendar.updateCells()
+        self.from_calendar.repaint()
+    elif sender == 'to_calendar':
+        self.to_calendar_label.setText(datestr)
+        self.to_calendar_label.repaint()
+        self.to_calendar.updateCells()
+        self.to_calendar.repaint()
     else:
         print('no sender')
     QApplication.processEvents()
 
 
-def calendar_get_dates(self) -> Tuple[date, date]:
-    date1: date = self.cal1.selectedDate().toPython()
-    date2: date = self.cal2.selectedDate().toPython()
-    return date1, date2
+def clear_interface(self) -> None:
+    self.browser.clear
+    self.progress_label.setText(' 0 / 0       ')
 
 
 
-# def calendar_refresh(self, year: int, month: int) -> None:
-#     sender: str = self.sender().accessibleName()
-#     qdate: QDate = QDate(year, month, 1)
-#     qdatestr: str = qdate.toString()
-#     if sender == 'cal1':
-#         self.cal1.setSelectedDate(qdate)
-#         self.cal1.updateCells()
-#         self.lb_cal1.setText(qdatestr)
-#     elif sender == 'cal2':
-#         self.cal2.setSelectedDate(qdate)
-#         self.cal2.updateCells()
-#         self.lb_cal2.setText(qdatestr)
+class MakeConnects:
+    """
+    I deliberate put MakeConnect class definition after all outer classes just to have a clearer flow.
+    """
+    def __init__(ego, self) -> None:
+
+        self.from_reset_button.clicked.connect(self.reset_calendar)
+        self.to_reset_button.clicked.connect(self.reset_calendar)
+        self.from_reset_button.click()
+        self.to_reset_button.click()
+
+        self.from_calendar.clicked[QDate].connect(self.update_calendar_date)
+        self.to_calendar.clicked[QDate].connect(self.update_calendar_date)
+
+        self.from_calendar.currentPageChanged.connect(self.from_calendar.repaint)
+        self.to_calendar.currentPageChanged.connect(self.to_calendar.repaint)
+
+        self.clear_button.clicked.connect(self.clear_interface)
+        self.quit_button.clicked.connect(self.close)
 
 
 
 class PriceUpdateView(QWidget):
     def __init__(self) -> None:
         super().__init__()
-
-        self.setWindowTitle('Stock Price Update')
-        self.setGeometry(0, 0, 900, 900)
-        self.mainbox = QVBoxLayout(self)
-
+        SetupWindow(self)  # mainbox is in SetupWindow() 
+        BrowserRow(self)
         CalendarGrid(self)
         UpdateGrid(self)
-        BrowserRow(self)
         ProgressRow(self)
         MakeConnects(self)
 
     def closeEvent(self, event: QCloseEvent) -> None:
         return closeEvent(self, event)
 
-    def calendar_reset(self) -> None:
-        return calendar_reset(self)
+    def reset_calendar(self) -> None:
+        return reset_calendar(self)
     
-    def calendar_show_date(self, qdate: QDate) -> None:
-        return calendar_show_date(self, qdate)
+    def update_calendar_date(self, qdate: QDate) -> None:
+        return update_calendar_date(self, qdate)
     
-    def calendar_get_dates(self) -> Tuple[date, date]:
-        return calendar_get_dates(self)
-
-
+    def clear_interface(self) -> None:
+        return clear_interface(self)
 
 
 
 
 def main() -> None:
     app = QApplication(sys.argv)
-    win = DailyStockWin()
+    win = PriceUpdateView()
     win.show()
     sys.exit(app.exec())
 
