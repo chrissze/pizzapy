f50c3c4b1bbd1aa9c18a2aa7ee3b9fb09815f1b7
CS <you@example.com>
2023-10-04 17:25:20 +0800

    pro

--- general_update/qt_model.py
+++ general_update/qt_model.py
@@ -2,10 +2,12 @@
 
 USED BY: core_browser_controller.py
 
-Custom Sort Filter Example:
-https://doc.qt.io/qtforpython-6/overviews/qtwidgets-itemviews-customsortfiltermodel-example.html
+QSortFilterProxyModel API:
+    https://doc.qt.io/qtforpython-6/PySide6/QtCore/QSortFilterProxyModel.html
 
-https://doc.qt.io/qtforpython-6/PySide6/QtCore/QSortFilterProxyModel.html#qsortfilterproxymodel
+Custom Sort Filter Example:
+    https://doc.qt.io/qtforpython-6/overviews/qtwidgets-itemviews-customsortfiltermodel-example.html
+
 
 """
 # STANDARD LIBS
@@ -24,15 +26,19 @@ from batterypy.string.read import is_floatable
 
 class MySortFilterProxyModel(QSortFilterProxyModel):
     """
-    This is for PySide6
+    USED BY: core_browser_controller.py
+
+    filterAcceptsRow() provides filtering capabilities for dock on the left.
+    lessThan() provides sorting capability for the TableView
     """
     def __init__(self) -> None:
         super().__init__()
         self.filters_dict: Dict[Union[int, str], QRegularExpression] = {}
 
-
     def setFilterByColumn(self, regex: QRegularExpression, column: int) -> None:
         """
+        USED BY: core_browser_controller.py
+
             This setFilterByColumn method is NOT a built-in method, I can use whatever name I like.
 
             The aim of this method is to fill up self.filters_dict, which is used by filterAcceptsRow() builtin virtual function.
@@ -64,12 +70,14 @@ class MySortFilterProxyModel(QSortFilterProxyModel):
         """
         This filterAcceptsRow() method is a built-in virtual function, I override it here, the name cannot be changed.
 
+        This method determines if a particular row in the TableView will be shown. If this method return True, the row will be shown, if this method return False, the row will be hidden.
+
         I use two lineedits to input lower limits and upper limits to filter QTableView results. 
         In order to distinguish if it is a lower limit or upper limit, I add CaseInsensitveOption to the floor_lineedit regular expression,
          
         In setFilterByColumn() method, I set the column key into a string when the regex is_floor_filter, so I need to use int(key) to convert the key back to an int in the else clause in the for loop below. 
 
-        'key, regex' is the key value pairs in self.filters_dict dictionary, that defined earlier.
+        'key, regex' is the key value pairs in self.filters_dict dictionary, defined in the custom class.
         
         True means SHOWING
 
@@ -105,21 +113,21 @@ class MySortFilterProxyModel(QSortFilterProxyModel):
                     #print(f' {result} {regex_text} {cell_text}')
                     if result:
                         return False
-                    
-
         return True   # The row is shown by default value True.
 
-    def lessThan(self, left: QModelIndex, right: QModelIndex) -> bool:
+    def lessThan(self, source_left: QModelIndex, source_right: QModelIndex) -> bool:
         """
+        lessThan is a built-in method.
+        
         When I click the column header text, this function will be triggered.
         """
-        leftstr: str = left.data()
-        rightstr: str = right.data()
+        left_str: str = source_left.data()
+        right_str: str = source_right.data()
 
-        if is_floatable(leftstr) and is_floatable(rightstr):
-            return float(leftstr) < float(rightstr) 
+        if is_floatable(left_str) and is_floatable(right_str):
+            return float(left_str) < float(right_str) 
         else:
-            return leftstr < rightstr
+            return left_str < right_str
 
 
 
