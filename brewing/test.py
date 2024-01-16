71a65808f2406debfe0fe4b6799f02d12214fa69
CS <you@example.com>
2024-01-16 10:27:10 +0800

    pro

--- brewing/test.py
+++ brewing/test.py
@@ -6,7 +6,10 @@ test relative import
 
 """
 
-from ..database_update.generated_stock_list import nasdaq_100_stocks
+
+fruits = ['apple', 'banana']
+
+
 
 if __name__ == '__main__':
     print(nasdaq_100_stocks)
