08eb234b9125aa86d320de1272d6379f138d6ff4
CS <you@example.com>
2025-12-31 17:51:14 +0800

    minor update

--- tests/test_pg_app_connection.py
+++ tests/test_pg_app_connection.py
@@ -2,7 +2,7 @@
 
 MAKE SURE I HAVE THE FOLLOWING ENV VARIABLES IN prompts.sh, THEY ARE asyncpg DEFAULT SYSTEM ENV VARIABLES FOR CONNECTION:
 
-    export PGHOST='fuji.220122.com' or 'localhsot'  (UPDATE)
+    export PGHOST='fuji.220122.xyz' or 'localhsot'  (UPDATE)
     export PGPORT=5432 
     export PGDATABASE='mydb'
     export PGUSER='postgres'
@@ -19,8 +19,24 @@ import asyncpg
 
 import pytest
 
+def test_asyncpg_env() -> None:
+    
+    pghost = os.environ.get('PGHOST')
+
+    pgport = os.environ.get('PGPORT')
+
+    pgdatabase = os.environ.get('PGDATABASE')
+    pguser = os.environ.get('PGUSER')
+
+    assert pghost in ['localhost', 'fuji.220122.xyz'], f"Invalid PGHOST: {pghost} "
+    assert pgport == '5432', f"Invalid PGPORT: {pgport} "
+    assert pgdatabase == 'mydb', f"Invalid PGDATABASE: {pgdatabase} "
+    assert pguser == 'postgres', f"Invalid PGDATABASE: {pguser} "
+
+
+
 @pytest.mark.asyncio
-async def test_pg() -> None:
+async def test_pg_connection() -> None:
     
     e = None
     conn = None
