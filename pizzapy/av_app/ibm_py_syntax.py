36867be3e031fa29e83b136187f1a2aa52121ac5
macbook13 <3chris@gmail.com>
2026-01-11 14:15:31 +0800

    minor update

--- pizzapy/av_app/ibm_py_syntax.py
+++ pizzapy/av_app/ibm_py_syntax.py
@@ -1,24 +1,25 @@
-import os
-
-
-import pandas as pd
-
-from pprint import pprint
-
-from alpha_vantage.options import Options  # pip install alpha_vantage
-
-API_KEY = os.getenv("AV_API_KEY")
-
-opt = Options(key=API_KEY, output_format="json")
+"""
 
 # Equivalent to:
 # https://www.alphavantage.co/query?function=HISTORICAL_OPTIONS&symbol=IBM&apikey=demo
 
-data: pd.DataFrame
 
-data, meta_data = opt.get_historical_options(symbol="SPY")
+"""
 
+import os
 
-print(data)
+import pandas as pd
 
-print(type(data))
+from alpha_vantage.options import Options
+
+API_KEY = os.getenv("AV_API_KEY")
+
+def get_chain(symbol: str) -> pd.DataFrame:
+
+    opt = Options(key=API_KEY, output_format='pandas')
+
+    data, _ = opt.get_historical_options(symbol=symbol)
+
+    return data
+
+print(get_chain('AMD'))
